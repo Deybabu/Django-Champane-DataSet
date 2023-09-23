1ce2067c1967d3c5aaaf2a2771e7dec5966f076d
Rajib Dey <79751619+Deybabu@users.noreply.github.com>
2023-09-23 23:08:17 +0530

    Update settings.py

--- AirPassanger/AirPassanger/settings.py
+++ AirPassanger/AirPassanger/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+sy-3&wf$1&sknvkt^r#%d14p4^%mfoiyqm*wun(rr&8%ch7r0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15.206.194.47','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 
